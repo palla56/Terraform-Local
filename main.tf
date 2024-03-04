089d4e9dd7e8ac9d532eb06ed70aef07e9051655
palla56 <pallakasbe56@gmail.com>
2024-03-04 16:24:21 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform { 
+    required_providers { 
+        docker = {
+         source = "kreuzwerker/docker" 
+        version = ">= 2.0.0" 
+        }
+    }
+}
+
+provider "docker" {
+    host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_container" "example" {
+        name = "my-container"
+        image = "nginx:latest" 
+}
+
